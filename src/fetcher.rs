4348baacd7f25cb77219eefb499338334214627a
Daniel Kral <dakral@protonmail.com>
2024-02-14 18:23:49 +0100

    Implement persistence for API responses

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -1,7 +1,10 @@
 use crate::markets::billa::BillaSearchResponse;
 use crate::markets::spar::SparSearchResponse;
+use brotli::CompressorWriter;
 use clap::ValueEnum;
 use reqwest::Client;
+use std::fs::File;
+use std::io::Write;
 
 const BILLA_API_URL: &str = "https://shop.billa.at/api/products/search/*?storeId=00-10";
 const SPAR_API_URL: &str = "https://search-spar.spar-ics.com/fact-finder/rest/v5/search/products_lmos_at?query=*&q=*&page=1&hitsPerPage=1000";
@@ -14,20 +17,51 @@ pub enum FetchSourceType {
     Spar,
 }
 
-pub async fn fetch_billa() -> Result<BillaSearchResponse, reqwest::Error> {
-    let client = Client::new();
-    let response = client.get(BILLA_API_URL).send().await?;
-
-    let response_json = response.json::<BillaSearchResponse>().await?;
-
-    Ok(response_json)
+pub enum FetchPersistType {
+    Json,
+    JsonBrotli,
 }
 
-pub async fn fetch_spar() -> Result<SparSearchResponse, reqwest::Error> {
+async fn save_fetch(
+    content: &String,
+    source: FetchSourceType,
+    target: FetchPersistType,
+) -> std::io::Result<()> {
+    let date = chrono::Utc::now().format("%Y-%m-%d");
+
+    match target {
+        FetchPersistType::Json => {
+            let filename = format!("{:?}-{}.json", source, date);
+            let mut output = File::create(filename)?;
+            output.write_all(content.as_bytes())
+        }
+        FetchPersistType::JsonBrotli => {
+            let filename = format!("{:?}-{}.json.br", source, date);
+            let mut output = File::create(filename)?;
+            let mut writer = CompressorWriter::new(&mut output, 4096, 11, 22);
+            writer.write_all(content.as_bytes())
+        }
+    }
+}
+
+pub async fn fetch_billa() -> reqwest::Result<BillaSearchResponse> {
+    let client = Client::new();
+    let response = client.get(BILLA_API_URL).send().await?;
+    let body = response.text().await?;
+    save_fetch(&body, FetchSourceType::Billa, FetchPersistType::Json)
+        .await
+        .unwrap();
+    let json = serde_json::from_str(&body).unwrap();
+    Ok(json)
+}
+
+pub async fn fetch_spar() -> reqwest::Result<SparSearchResponse> {
     let client = Client::new();
     let response = client.get(SPAR_API_URL).send().await?;
-
-    let response_json = response.json::<SparSearchResponse>().await?;
-
-    Ok(response_json)
+    let body = response.text().await?;
+    save_fetch(&body, FetchSourceType::Spar, FetchPersistType::Json)
+        .await
+        .unwrap();
+    let json = serde_json::from_str(&body).unwrap();
+    Ok(json)
 }

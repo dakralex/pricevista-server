bec13c2f92a3c396b7bd5d3e9c6857598473321f
Daniel Kral <dakral@protonmail.com>
2024-02-27 12:33:26 +0100

    Implement price information for Billa deserializer; Improve Billa API fetch and deserialization

--- src/providers/billa.rs
+++ src/providers/billa.rs
@@ -1,20 +1,22 @@
 use crate::parser;
 use crate::providers::{Fetch, Merge};
 use reqwest::Client;
-use serde::Deserialize;
-use std::fmt::format;
+use serde::{Deserialize, Deserializer};
+use std::str::FromStr;
 
 pub struct BillaShopApi;
 
 impl BillaShopApi {
+    const PAGE_SIZE: &'static str = "500";
+    const STORE_ID: &'static str = "00-10";
+
     async fn request(client: &Client, page: usize) -> crate::error::Result<BillaSearchResponse> {
         let url = format!("{}/products", Self::API_BASE_URL);
-        let page = format!("{}", page);
 
         let request = client.get(&url).query(&[
-            ("page", page.as_str()),
-            ("pageSize", "500"),
-            ("storeId", "00-10"),
+            ("page", page.to_string().as_str()),
+            ("pageSize", Self::PAGE_SIZE),
+            ("storeId", Self::STORE_ID),
         ]);
         let response = request.send().await?;
         Ok(response.json::<BillaSearchResponse>().await?)
@@ -29,7 +31,12 @@ impl Fetch for BillaShopApi {
         let mut page = 0;
         let mut total = Self::request(&client, page).await?;
 
-        while &total.total > &0 {
+        while let Some(count) = &total.count {
+            // TODO Find out if this check is needed
+            if count == &0 {
+                break;
+            }
+
             page += 1;
             total.merge(Self::request(&client, page).await?);
         }
@@ -41,15 +48,22 @@ impl Fetch for BillaShopApi {
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct BillaSearchResponse {
-    count: usize,
+    /// The count of the items that are contained in results (if any):
+    count: Option<usize>,
+
+    /// The total amount of item the request produces (through paging).
     total: usize,
+
+    /// The results from the request.
     results: Vec<BillaProduct>,
-    facets: Vec<BillaResultFacet>,
+
+    /// The facets that are relevant for the request.
+    facets: Vec<BillaFacet>,
 }
 
 impl Merge for BillaSearchResponse {
     fn merge(&mut self, rhs: Self) {
-        self.count += rhs.count;
+        self.count = rhs.count;
         assert_eq!(self.total, rhs.total);
         self.results.extend(rhs.results);
         // TODO Make facets a HashSet or reimplement Extend/Trait on it
@@ -64,7 +78,7 @@ pub struct BillaProduct {
     #[serde(with = "parser::double_as_string")]
     amount: f64,
     badges: Option<Vec<BillaBadge>>,
-    pub brand: Option<BillaBrand>,
+    brand: Option<BillaBrand>,
     category: Option<String>,
     country_of_origin: Option<String>,
     deposit_type: Option<String>,
@@ -76,12 +90,12 @@ pub struct BillaProduct {
     max_quantity: Option<u32>,
     medical: bool,
     min_quantity: Option<u32>,
-    pub name: String,
+    name: String,
     package_label: Option<String>,
     package_label_key: Option<String>,
     parent_categories: Vec<Vec<BillaParentCategory>>,
     preparation_time: Option<u32>,
-    pub price: BillaPriceInfo,
+    price: BillaPriceInfo,
     product_id: String,
     product_marketing: Option<String>,
     purchased: bool,
@@ -161,6 +175,10 @@ enum BillaBadge {
     /// European Union's organic food certification
     #[serde(rename = "gs-eubio")]
     EUBioMark,
+
+    /// Austrian "Fair zum Tier" animal certification
+    #[serde(rename = "gs-fairzumtier")]
+    FairZumTier,
 
     /// International Forest Stewardship Council certification
     #[serde(rename = "gs-fsc")]
@@ -268,8 +286,83 @@ enum BillaBadge {
     VegetarianProduct,
 }
 
+/// Type that identifies prices at the Billa Online Shop.
+///
+/// The prices are given in integers, that means they are in the minor unit of
+/// the currency (here it is very likely to be always EUR) and converted in
+/// further usages to a decimal representation.
 #[derive(Debug, Deserialize)]
-struct BillaPriceInfo {}
+struct BillaPrice(u32);
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct BillaPriceInfo {
+    base_price_factor: Option<String>,
+    base_unit_long: String,
+    base_unit_short: String,
+    crossed: Option<BillaPrice>,
+    discount_percentage: Option<i32>,
+    lowest_price: Option<BillaPrice>,
+    regular: BillaPriceItem,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct BillaPriceItem {
+    per_standardized_quantity: BillaPrice,
+    promotion_quantity: Option<BillaPrice>,
+    promotion_text: Option<String>,
+    promotion_type: Option<BillaPromotionType>,
+    promotion_value: Option<BillaPrice>,
+    promotion_value_per_standardized_quantity: Option<BillaPrice>,
+    tags: Vec<BillaPromotionTag>,
+    value: BillaPrice,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+enum BillaPromotionType {
+    From,
+    PerSetOf,
+}
+
+#[derive(Debug)]
+enum BillaPromotionTag {
+    Discount,
+    Bogo(u32, u32),
+    Unknown,
+}
+
+impl BillaPromotionTag {
+    fn extract_bogo_vals(term: &str) -> BillaPromotionTag {
+        let nums: Vec<&str> = term.split("plus").collect();
+        assert_eq!(nums.len(), 2);
+
+        match (u32::from_str(nums[0]), u32::from_str(nums[1])) {
+            (Ok(x), Ok(y)) => Self::Bogo(x, y),
+            _ => Self::Unknown,
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for BillaPromotionTag {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+
+        if let Some(suffix) = value.strip_prefix("pt-") {
+            match suffix {
+                "abverkauf" | "aktion" | "extrem" | "satterrabatt" => Ok(Self::Discount),
+                s if s.contains("plus") => Ok(Self::extract_bogo_vals(s)),
+                _ => Ok(Self::Unknown),
+            }
+        } else {
+            Err(serde::de::Error::custom("Invalid Billa promotion tag"))
+        }
+    }
+}
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
